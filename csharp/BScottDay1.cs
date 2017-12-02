e3f0d39e02a4bb55b54560c71a9b6b0b72bc0f0f
Brandon Scott <xeon.productions@gmail.com>
2017-12-02 01:29:46 -0600

    Day 1 and 2 commit

---
+++ csharp/BScottDay1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace AOC2017
+{
+    class BScottDay1
+    {
+        static void Main(string[] args)
+        {
+            string input = "1122";
+            Console.WriteLine($"Example 1: {CalculateSumPart1(input)}");
+
+            input = "1111";
+            Console.WriteLine($"Example 2: {CalculateSumPart1(input)}");
+
+            input = "1234";
+            Console.WriteLine($"Example 3: {CalculateSumPart1(input)}");
+
+            input = "91212129";
+            Console.WriteLine($"Example 4: {CalculateSumPart1(input)}");
+
+            input = File.ReadAllText("BScottDay1.txt");
+            Console.WriteLine($"Part 1 Answer: {CalculateSumPart1(input)}");
+            Console.WriteLine($"Part 2 Answer: {CalculateSumPart2(input)}");
+
+            Console.ReadLine();
+        }
+
+        static long CalculateSumPart1(string input)
+        {
+            long sum = 0;
+            for (int i = 0; i < input.Length; i++)
+                if (input[i] == input[(i + 1) % input.Length])
+                    sum += int.Parse(input[i].ToString());
+            return sum;
+        }
+
+        static long CalculateSumPart2(string input)
+        {
+            long sum = 0;
+            int step = input.Length / 2;
+            for (int i = 0; i < input.Length; i++)
+                if (input[i] == input[(i + step) % input.Length])
+                    sum += int.Parse(input[i].ToString());
+            return sum;
+        }
+    }
+}
---
+++ csharp/BScottDay2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AOC2017
+{
+    class BScottDay2
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("BScottDay2.txt");
+            Console.WriteLine($"Part 1 Answer: {CalculateChecksumPart1(input)}");
+            Console.WriteLine($"Part 2 Answer: {CalculateChecksumPart2(input)}");
+            Console.ReadLine();
+        }
+
+        static long CalculateChecksumPart1(string[] rows)
+        {
+            long checksum = 0;
+            for (int i = 0; i < rows.Length; i++)
+            {
+                int min = int.MaxValue, max = 0;
+                int[] cols = Array.ConvertAll(rows[i].Split(new char[] {'\t'}), s => int.Parse(s));
+                for (int j = 0; j < cols.Length; j++)
+                {
+                    if (cols[j] < min)
+                        min = cols[j];
+                    if (cols[j] > max)
+                        max = cols[j];
+                }
+                checksum += Math.Abs(max - min);
+            }
+            return checksum;
+        }
+
+        static long CalculateChecksumPart2(string[] rows)
+        {
+            long checksum = 0;
+            for (int i = 0; i < rows.Length; i++)
+            {
+                int value = 0;
+                int[] cols = Array.ConvertAll(rows[i].Split(new char[] { '\t' }), s => int.Parse(s));
+                for (int j = 0; j < cols.Length; j++)
+                {
+                    for (int k = 0; k < cols.Length; k++)
+                    {
+                        if (cols[j] > cols[k] && (cols[j] % cols[k]) == 0)
+                        {
+                            value = cols[j] / cols[k];
+                            break;
+                        }
+                    }
+                    if (value > 0)
+                        break;
+                }
+                checksum += value;
+            }
+            return checksum;
+        }
+    }
+}
