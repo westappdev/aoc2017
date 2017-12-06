cd5ccf9b034f5b58c8f45f0fb1224ed0f3fd1edf
Brandon Scott <xeon.productions@gmail.com>
2017-12-05 23:00:25 -0600

    Day 3 Part 1 commit.

---
+++ csharp/BScottDay3.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2017
+{
+    class BScottDay3
+    {
+        static void Main(string[] args)
+        {
+            int value = 1;
+            Console.WriteLine($"Value: {value}, Distance: {GetDistanceToAccessPortPart1(value)}");
+
+            value = 12;
+            Console.WriteLine($"Value: {value}, Distance: {GetDistanceToAccessPortPart1(value)}");
+
+            value = 23;
+            Console.WriteLine($"Value: {value}, Distance: {GetDistanceToAccessPortPart1(value)}");
+
+            value = 1024;
+            Console.WriteLine($"Value: {value}, Distance: {GetDistanceToAccessPortPart1(value)}");
+
+            value = 325489;
+            Console.WriteLine($"Value: {value}, Distance: {GetDistanceToAccessPortPart1(value)}");
+
+            Console.ReadLine();
+        }
+
+        static int GetDistanceToAccessPortPart1(int value)
+        {
+            Point start = new Point(0, 0);
+            Point x = GetPosition(value);
+            return Math.Abs(start.X - x.X) + Math.Abs(start.Y - x.Y);
+        }
+
+        // Inverse ulam spiral coords
+        static Point GetPosition(int n)
+        {
+            double k = Math.Ceiling((Math.Sqrt(n) - 1) / 2);
+            double t = 2 * k + 1;
+            double m = Math.Pow(t, 2);
+
+            t = t - 1;
+            if (n >= m - t)
+                return new Point((int)(k - (m - n)), (int)-k);
+
+            m = m - t;
+            if (n >= m - t)
+                return new Point((int)-k, (int)(-k + (m - n)));
+
+            m = m - t;
+            if (n >= m - t)
+                return new Point((int)(-k + (m - n)), (int)k);
+
+            return new Point((int)k, (int)(k - (m - n - t)));
+        }
+
+        struct Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Point(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+        }
+    }
+}
