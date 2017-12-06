dc194d9d529e752574f0c9db570e38829e67d075
Brandon Scott <xeon.productions@gmail.com>
2017-12-05 22:59:23 -0600

    Day 4 commit

---
+++ csharp/BScottDay4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace AOC2017
+{
+    class BScottDay4
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("BScottDay4.txt");
+            Console.WriteLine($"Part 1 Answer: {GetValidPassPhrasesPart1(input)}");
+            Console.WriteLine($"Part 2 Answer: {GetValidPassPhrasesPart2(input)}");
+            Console.ReadLine();
+        }
+
+        static int GetValidPassPhrasesPart1(string[] input)
+        {
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] passStrings = input[i].Split(new char[] {' '});
+                string[] distinctStrings = passStrings.Distinct().ToArray();
+                if (passStrings.Length == distinctStrings.Length)
+                    count++;
+            }
+            return count;
+        }
+
+        static int GetValidPassPhrasesPart2(string[] input)
+        {
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] passStrings = Array.ConvertAll( input[i].Split(new char[] { ' ' }), s => string.Concat(s.OrderBy(c => c)));
+                string[] distinctStrings = passStrings.Distinct().ToArray();
+                if (passStrings.Length == distinctStrings.Length)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
