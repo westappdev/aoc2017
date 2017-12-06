470f18fe3ba168e7b522bc79c7b301126459866e
Brandon Essler <Essler@users.noreply.github.com>
2017-12-06 01:05:17 -0600

    Removed extra print statement.

--- swift/EsslerDay1A.swift
+++ swift/EsslerDay1A.swift
@@ -3,11 +3,10 @@ let length = input.count
 let digits = Array(input)
 var sum = 0
 
-print (digits)
-
 for index in 0..<length { 
   if digits[index] == digits[(index+1)%length] {
     sum += Int(String(digits[index]))!
   }
 }
+
 print(sum)
