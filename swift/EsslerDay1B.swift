8d4bf47d6a909bd15558356e450fcec9906d5cb5
Brandon Essler <Essler@users.noreply.github.com>
2017-12-06 01:03:22 -0600

    Create EsslerDay1B.swift

---
+++ swift/EsslerDay1B.swift
@@ -0,0 +1,13 @@
+let input = "12131415"
+let length = input.count
+let digits = Array(input)
+var sum = 0
+
+print (digits)
+
+for index in 0..<length { 
+  if digits[index] == digits[(index+length/2)%length] {
+    sum += Int(String(digits[index]))!
+  }
+}
+print(sum)
